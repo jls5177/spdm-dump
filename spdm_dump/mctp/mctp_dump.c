fc8e93a9e9305b39b4d0c5b9d652f7afdf402b56
Justin Simon <justin.simon@microsoft.com>
2023-08-10 20:43:28 -0500

    support fragmented MCTP messages
    This commit reassembled fragmented MCTP payloads before processing
    the message.

--- spdm_dump/mctp/mctp_dump.c
+++ spdm_dump/mctp/mctp_dump.c
@@ -6,6 +6,9 @@
 
 #include "spdm_dump.h"
 
+void *m_reassembly_data_buffer;
+uint32_t data_buffer_size = 0;
+
 dispatch_table_entry_t m_mctp_dispatch[] = {
     { MCTP_MESSAGE_TYPE_MCTP_CONTROL, "MctpControl", NULL },
     { MCTP_MESSAGE_TYPE_PLDM, "PLDM", dump_pldm_message },
@@ -17,6 +20,12 @@ dispatch_table_entry_t m_mctp_dispatch[] = {
       dump_secured_spdm_message },
     { MCTP_MESSAGE_TYPE_VENDOR_DEFINED_PCI, "VendorDefinedPci", NULL },
     { MCTP_MESSAGE_TYPE_VENDOR_DEFINED_IANA, "VendorDefinedIana", NULL },
+};
+
+value_string_entry_t m_mctp_transport_header_flag_string_table[] = {
+        {MCTP_HDR_FLAG_TO,  "TO"},
+        {MCTP_HDR_FLAG_SOM, "SOM"},
+        {MCTP_HDR_FLAG_EOM, "EOM"},
 };
 
 void dump_mctp_message(const void *buffer, size_t buffer_size)
@@ -56,15 +65,53 @@ void dump_mctp_message(const void *buffer, size_t buffer_size)
     }
 }
 
-void dump_mctp_packet(const void *buffer, size_t buffer_size)
-{
+void dump_mctp_packet(const void *buffer, size_t buffer_size) {
     size_t header_size;
+    mctp_header_t *hdr;
 
     header_size = sizeof(mctp_header_t);
     if (buffer_size < header_size) {
         return;
     }
 
-    dump_mctp_message((uint8_t *)buffer + header_size,
-                      buffer_size - header_size);
+    hdr = (mctp_header_t *) buffer;
+
+    // buffer contains entire message and can be directly processed
+    if ((hdr->message_tag & (MCTP_HDR_FLAG_SOM | MCTP_HDR_FLAG_EOM)) == (MCTP_HDR_FLAG_SOM | MCTP_HDR_FLAG_EOM)) {
+        dump_mctp_message((uint8_t *) buffer + header_size,
+                          buffer_size - header_size);
+        return;
+    }
+
+    // message is fragmented, need to reassemble the message before processing the buffer
+
+    size_t starting_offset = header_size;
+    if (hdr->message_tag & MCTP_HDR_FLAG_SOM) {
+        m_reassembly_data_buffer = (void *) malloc(get_max_packet_length());
+    }
+    if (m_reassembly_data_buffer == NULL) {
+        printf("!!!memory out of resources!!!\n");
+        return;
+    }
+
+    size_t fragment_size = buffer_size - starting_offset;
+    memcpy(m_reassembly_data_buffer + data_buffer_size, buffer + starting_offset, fragment_size);
+    data_buffer_size += fragment_size;
+
+    if (hdr->message_tag & MCTP_HDR_FLAG_EOM) {
+        dump_mctp_message((uint8_t *) m_reassembly_data_buffer, data_buffer_size);
+        free(m_reassembly_data_buffer);
+        m_reassembly_data_buffer = NULL;
+        data_buffer_size = 0;
+    } else {
+        printf("FRAG(");
+        if (!m_param_quite_mode) {
+            printf("MsgType=%u, Tag=%u, Seq#=%u, ", *(uint8_t *) m_reassembly_data_buffer, hdr->message_tag & 0x7,
+                   (hdr->message_tag & 0x30) >> 4);
+            dump_entry_flags_all(m_mctp_transport_header_flag_string_table,
+                                 LIBSPDM_ARRAY_SIZE(m_mctp_transport_header_flag_string_table),
+                                 hdr->message_tag);
+        }
+        printf(")\n");
+    }
 }
